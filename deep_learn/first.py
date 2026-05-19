84c35325bba3d5b574c7d3b0662a0ca5f1adb96e
gorgick <gorgick2014@yandex.by>
2026-05-19 14:40:53 +0300

    w_sum with numpy

--- deep_learn/first.py
+++ deep_learn/first.py
@@ -11,4 +11,12 @@ def neural_network(input, weight):
     return prediction
 
 
+weights = [0.1, 0.2, 0]
 
+toes = [8.5, 9.5, 9.9, 9.0]
+wlrec = [0.65, 0.8, 0.8, 0.9]
+nfans = [1.2, 1.3, 0.5, 1.0]
+
+input = [toes[0], wlrec[0], nfans[0]]
+pred = neural_network(input, weights)
+print(pred)
