e43b180275af3f7040dcab1b15757aa9a7e15b90
gorgick <gorgick2014@yandex.by>
2026-07-10 23:09:25 +0300

    cold/hot method to change error

--- deep_learn/learning_2.py
+++ deep_learn/learning_2.py
@@ -5,3 +5,40 @@ goal_pred = 0.8  # Значение которое мы хотели бы вид
 pred = input * knob_weight
 error = (pred - goal_pred) ** 2  # Среднеквадратичная ошибка
 print(error)
+
+"""Метод холодно/горячо, способ уменьшения вероятности ошибки"""
+
+weight = 0.1
+
+
+def neural_network(input, weight):
+    prediction = input * weight
+    return prediction
+
+
+number_of_toes = [8.5]
+win_or_lose_binary = [1]  # Победа
+
+input = number_of_toes[0]
+true = win_or_lose_binary[0]
+pred = neural_network(input, weight)
+error = (pred - true) ** 2
+print(error)
+
+lr = 0.1
+p_up = neural_network(input, weight) + lr  # Приращение
+e_up = (p_up - true) ** 2
+print(e_up)
+
+lr = 0.01
+p_dn = neural_network(input, weight) - lr  # Уменьшение
+e_dn = (p_dn - true) ** 2
+print(e_dn)
+
+if (error > e_dn or error > e_up):
+    if (e_dn < e_up):
+        weight -= lr
+    if (e_up < e_dn):
+        weight += lr
+
+print(weight)
