48474853e50f6f559a40f776325f5223339e7218
gorgick <gorgick2014@yandex.by>
2026-08-13 21:28:23 +0300

    cold/hot method to change error, rebuilded

--- deep_learn/learning_2.py
+++ deep_learn/learning_2.py
@@ -23,17 +23,19 @@ input = number_of_toes[0]
 true = win_or_lose_binary[0]
 pred = neural_network(input, weight)
 error = (pred - true) ** 2
-print(error)
+print("error - ", error)
 
 lr = 0.1
 p_up = neural_network(input, weight) + lr  # Приращение
 e_up = (p_up - true) ** 2
-print(e_up)
+print("e_up - ", e_up)
 
 lr = 0.01
 p_dn = neural_network(input, weight) - lr  # Уменьшение
 e_dn = (p_dn - true) ** 2
-print(e_dn)
+print("e_dn - ", e_dn)
+
+print("weight", weight)
 
 if (error > e_dn or error > e_up):
     if (e_dn < e_up):
@@ -41,4 +43,4 @@ if (error > e_dn or error > e_up):
     if (e_up < e_dn):
         weight += lr
 
-print(weight)
+print("weight", weight)
